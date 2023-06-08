fe3f1e3d949aa93334038fbb2a7c92ec27d7f410
jrubenoff <josh@rubenoff.com>
2023-06-08 15:21:55 -0700

    Add a “Key Questions” component

---
+++ stories/KeyQuestions.stories.js
@@ -0,0 +1,29 @@
+import "./scss/app.scss";
+
+export default {
+  title: 'Components/Key Questions',
+  args: {
+    'question1': 'How can the findings be turned into actionable solutions?',
+    'question2': 'What recommendations can be implemented immediately and what might be long-term future work?',
+    'question3': 'What recommendations will deliver the highest impact?',
+    'question4': 'What time, effort, resources, and expertise is required to implement the solutions?',
+    'question5': '',
+  },
+};
+
+export const KeyQuestions = (args) => `</div> <!-- /.l-stack-basic -->
+</div> <!-- /.l-text-wrapper -->
+<div class="l-section key-questions">
+  <div class="l-text-wrapper">
+    <h3 class="h4">Key Questions</h3>
+    <ul class="l-stack">
+      ${args.question1 ? `<li>${args.question1}</li>` : ``}
+      ${args.question2 ? `<li>${args.question2}</li>` : ``}
+      ${args.question3 ? `<li>${args.question3}</li>` : ``}
+      ${args.question4 ? `<li>${args.question4}</li>` : ``}
+      ${args.question5 ? `<li>${args.question5}</li>` : ``}
+    </ul>
+  </div>
+</div>
+<div class="l-text-wrapper">
+<div class="l-stack-basic">`;
