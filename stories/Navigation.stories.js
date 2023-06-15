48191726f262ac0ad501bd9cbcbb146c6005122d
jrubenoff <josh@rubenoff.com>
2023-06-14 22:34:59 -0700

    Update navigation styles

--- stories/Navigation.stories.js
+++ stories/Navigation.stories.js
@@ -19,6 +19,12 @@ const bootstrapIconSearch = `
 </svg>
 `;
 
+const bootstrapIconArrow90DegUp = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-up" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4.854 1.146a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L4 2.707V12.5A2.5 2.5 0 0 0 6.5 15h8a.5.5 0 0 0 0-1h-8A1.5 1.5 0 0 1 5 12.5V2.707l3.146 3.147a.5.5 0 1 0 .708-.708l-4-4z"/>
+</svg>
+`;
+
 export default {
   title: 'Layout/Navigation',
   args: {
@@ -36,8 +42,9 @@ export const Navigation = (args) => `
       </div>
 
       <div class="nav-buttons">
-        <button class="nav-search-button">${bootstrapIconSearch}Search</button>
-        <button id="all-sections-button">${bootstrapIconBook}All Chapters</button>
+        <button class="nav-button">${bootstrapIconArrow90DegUp}More Guides</button>
+        <button class="nav-button" id="all-sections-button">${bootstrapIconBook}Chapters</button>
+        <button class="nav-search-button">${bootstrapIconSearch}<span class="sr-only">Search</span></button>
       </div>
     </div>
   </nav>
