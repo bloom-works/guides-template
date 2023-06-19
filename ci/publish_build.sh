f6c5af8610cca15dcd69369ff3de5307f0cecd21
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2023-06-19 18:18:18 -0400

    Added scripts for running CI tasks

---
+++ ci/publish_build.sh
@@ -0,0 +1,10 @@
+# ARGS
+# $1: Version tag (i.e. "1.0.0")
+
+# Fail if any command exits with a non-zero exit code
+set -e 
+
+# -----
+# REPLACE THE BELOW WITH YOUR COMMANDS
+# -----
+echo "For version tag $1, build the artifacts and publish them (such as building your Docker container or packaging, then pushing to container repo or central package store)"
