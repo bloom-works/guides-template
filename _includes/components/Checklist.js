ff15faf95f6260394d7f7d4e1dd07fb026197ca4
jrubenoff <josh@rubenoff.com>
2023-06-26 16:42:20 -0700

    Consolidate Key Qs and Checklist to icon-list.css

--- _includes/components/Checklist.js
+++ _includes/components/Checklist.js
@@ -1,6 +1,7 @@
+const { html } = require('common-tags');
 function renderChecklist(content){
-  return `
-    <div class="l-section checklist">
+  return html`
+    <div class="l-section icon-list" data-icon-type="checklist">
       <h3 class="h4">Checklist</h3>
       <ul class="l-stack-basic">
         ${content}
--- _includes/components/KeyQuestions.js
+++ _includes/components/KeyQuestions.js
@@ -1,6 +1,7 @@
+const { html } = require("common-tags");
 function renderKeyQuestions(content){
-  return `
-    <div class="l-section key-questions">
+  return html`
+    <div class="l-section icon-list" data-icon-type="key-questions">
       <h3 class="h4">Key Questions</h3>
       <ul class="l-stack-basic">
         ${content}
