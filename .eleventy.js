42f6fa1af4f239500b0925925afb440f1cb446d1
jrubenoff <josh@rubenoff.com>
2023-06-26 11:55:00 -0700

    Add first draft of Callout component

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,8 @@ const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 
+const CalloutComponent = require("./_includes/components/Callout")
+
 module.exports = function(eleventyConfig) {
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
@@ -89,6 +91,8 @@ module.exports = function(eleventyConfig) {
     ui: false,
     ghostMode: false
   });
+
+  eleventyConfig.addPairedShortcode("Callout", CalloutComponent);
 
   return {
     // Control which files Eleventy will process
