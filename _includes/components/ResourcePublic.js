6f6cbb7f68c4aa6d1b121bc0554879fa93150f56
jrubenoff <josh@rubenoff.com>
2023-06-27 16:34:15 -0700

    Add title attrs to resource SVGs

--- _includes/components/ResourcePublic.js
+++ _includes/components/ResourcePublic.js
@@ -5,18 +5,21 @@ function renderResourcePublic(content, url){
   const resourceText = markdown.render(content);
 
   let icon = `link-45deg`;
+  let title = `Link`;
 
   if (url.includes('docs.google.com')) {
     icon = `file-earmark-text`;
+    title = `Google Doc`;
   } else if (url.endsWith('.pdf')) {
     icon = `filetype-pdf`;
+    title = `PDF`;
   }
 
   return html`
     <li>
       <a href="${url}">
         <span class="resource-icon">
-          <svg class="bi" width="1em" height="1em" fill="currentColor">
+          <svg class="bi" width="1em" height="1em" fill="currentColor" title="${title}">
             <use xlink:href="/bootstrap-icons.svg#${icon}"></use>
           </svg>
         </span>
