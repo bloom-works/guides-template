6c56163350bc91a743ceb3338a469e3c7e77fbac
jrubenoff <josh@rubenoff.com>
2023-06-27 13:56:46 -0700

    Add icons for different resource file types

--- _includes/components/ResourcePublic.js
+++ _includes/components/ResourcePublic.js
@@ -4,12 +4,20 @@ const markdown = require("../../markdown");
 function renderResourcePublic(content, url){
   const resourceText = markdown.render(content);
 
+  let icon = `link-45deg`;
+
+  if (url.includes('docs.google.com')) {
+    icon = `file-earmark-text`;
+  } else if (url.endsWith('.pdf')) {
+    icon = `filetype-pdf`;
+  }
+
   return html`
     <li>
       <a href="${url}">
         <span class="resource-icon">
           <svg class="bi" width="1em" height="1em" fill="currentColor">
-            <use xlink:href="/bootstrap-icons.svg#file-earmark-text"></use>
+            <use xlink:href="/bootstrap-icons.svg#${icon}"></use>
           </svg>
         </span>
         <span class="resource-text">${resourceText}</span>
