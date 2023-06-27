8f5d80e80bd784c1bc01a1765b886abaa982caf6
jrubenoff <josh@rubenoff.com>
2023-06-27 13:57:48 -0700

    Change resources layout from .l-cluster to .l-grid

--- _includes/components/ResourceGroup.js
+++ _includes/components/ResourceGroup.js
@@ -4,7 +4,7 @@ function renderResourceGroup(content, headline){
   return html`
     <h2>${headline || "Additional Resources"}</h2>
     <div class="resources">
-      <ul class="l-cluster">${content}</ul>
+      <ul class="l-grid">${content}</ul>
     </div>
   `
 }
