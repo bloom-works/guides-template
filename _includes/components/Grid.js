4c401d8380d0c4d2c8f12308b486ff1c2fed7782
Josh Rubenoff <1328849+jrubenoff@users.noreply.github.com>
2023-06-27 18:06:30 -0700

    Remove gratuitous padding from grid container

--- _includes/components/Grid.js
+++ _includes/components/Grid.js
@@ -3,7 +3,7 @@ const { html } = require('common-tags')
 function renderGrid(content){
 
   return html`
-    <div class="l-wrapper">
+    <div class="grid-wrapper">
       <div class="l-grid icon-grid">
         ${content}
       </div>
