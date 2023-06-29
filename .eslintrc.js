37dee8e5d313b03e21f34e593aa6ae16c22f32a8
Lynn Herrick <lynn@bloomworks.digital>
2023-06-29 15:26:19 -0700

    add eslint standard config for node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,26 @@
-// TODO .eslintrc.js example
 module.exports = {
-    "env": {
-        "browser": true,
-        //"es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    rules: {
-        'no-restricted-imports': [
-          'error',
-          {
-            paths: [
-              {
-                name: 'date-fns/locale',
-                message:
-                  "Please, import a specific locale instead, e.g. `import enUS from 'date-fns/locale/en-US';`",
-              },
-            ],
-            patterns: ['!date-fns/locale/*'],
-          },
-        ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [
+    {
+      env: {
+        node: true
       },
-    
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
   }
+}
