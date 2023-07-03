e29c13cf4791b7ff14af57136e3ea2ccf227d506
makaitakori <makai@bloomworks.digital>
2023-07-03 13:33:48 -0400

    took out instructions for local webserver, added in mobile testing for chrome and safari

--- playwright.config.js
+++ playwright.config.js
@@ -49,31 +49,14 @@ module.exports = defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+     {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+     },
+    {
+       name: 'Mobile Safari',
+       use: { ...devices['iPhone 12'] },
+     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
 
