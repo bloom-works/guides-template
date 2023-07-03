347d2f93d0f501d3591099f25575d5d32ec85640
makaitakori <makai@bloomworks.digital>
2023-07-03 12:23:44 -0400

    checking failing tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,7 +5,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Bloomworks/);
 });
 
 test('get started link', async ({ page }) => {
