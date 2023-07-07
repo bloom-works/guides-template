cc966d58c82ecc22387057ea6a2ac3ac63a468e6
makaitakori <makai@bloomworks.digital>
2023-07-07 14:06:27 -0400

    fix linter errors

--- _includes/components/Callout.js
+++ _includes/components/Callout.js
@@ -1,7 +1,7 @@
 const { html } = require('common-tags');
-const markdown = require("../../markdown");
+const markdown = require('../../markdown');
 
-function renderCallout(content, type){
+function renderCallout (content, type) {
   const text = markdown.render(content);
 
   let icon = '';
@@ -9,31 +9,31 @@ function renderCallout(content, type){
 
   switch (type) {
     case 'event':
-      icon = "calendar-week";
-      title = "Upcoming event";
+      icon = 'calendar-week';
+      title = 'Upcoming event';
       break;
     case 'pep-talk':
-      icon = "heart";
-      title = "A pep talk";
+      icon = 'heart';
+      title = 'A pep talk';
       break;
     case 'warning':
-      icon = "exclamation";
-      title = "Warning";
+      icon = 'exclamation';
+      title = 'Warning';
       break;
     default:
-      icon = "lightbulb";
-      title = "Important information";
+      icon = 'lightbulb';
+      title = 'Important information';
   }
 
   return html`
-    <section class="l-section callout" data-type="${type || "info"}">
-      <div class="callout-inner">
-        <span class="callout-icon">
-          <svg title="${ title }" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-            <use xlink:href="/bootstrap-icons.svg#${ icon }"></use>
+    <section class='l-section callout' data-type='${type || 'info'}'>
+      <div class='callout-inner'>
+        <span class='callout-icon'>
+          <svg title='${title}' xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 16 16'>
+            <use xlink:href='/bootstrap-icons.svg#${icon}'></use>
           </svg>
         </span>
-        <div class="l-stack-basic">${ text }</div>
+        <div class='l-stack-basic'>${text}</div>
       </div>
     </section>
   `
