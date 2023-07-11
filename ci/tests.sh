f4a78ffdaab2a441c15e1b3e224c8b21ef236482
Makai Takori <SMakaiTakori@users.noreply.github.com>
2023-07-11 15:16:29 -0400

    Update tests.sh

--- ci/tests.sh
+++ ci/tests.sh
@@ -10,4 +10,4 @@ npx playwright test
 echo 'Run accessibility tests'
 # Run Cypress accessibility tests
 apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
-npm cypress run
+npx cypress run
