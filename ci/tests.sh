9c4765e473384edd57a76a5d51fc9a6bf3de8166
makaitakori <makai@bloomworks.digital>
2023-07-10 20:04:23 -0400

    testing

--- ci/tests.sh
+++ ci/tests.sh
@@ -8,5 +8,5 @@ npx playwright install --with-deps
 npx playwright test
 
 # Run Cypress accessibility tests
-apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+# apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 npx cypress run
