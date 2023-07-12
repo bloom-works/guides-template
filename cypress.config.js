8c491516f837aa295a683ffaf2d024077fdb586e
Makai Takori <SMakaiTakori@users.noreply.github.com>
2023-07-11 20:16:15 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: null,
-    experimentalCspAllowList: false
+    experimentalSourceRewriting: false
   }
 });
