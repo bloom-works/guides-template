7c62ee681ca58bffd4b70abaf13eee66786f28b6
makaitakori <makai@bloomworks.digital>
2023-07-12 17:30:20 -0400

    code cleanup

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080/',
     // sets cypress' automatic video recording to false when cypress is run
-
     video: false
   }
 });
